04491f5b8b928f301a59a3c196bf00cf9e08f32d
reSHARMA <reshabhsh@gmail.com>
2020-01-18 18:50:24 +0000

    take care of soft float library calls

---
+++ support/dummy-libfloat.c
@@ -0,0 +1,23 @@
+/* This dummy file provides soft float support */
+/* The purpose of this library is to measure the size of code excluding target
+   dependent C library code. It only makes sense if it is used with
+   -gc-sections. */
+
+double
+__floatdidf (double x __attribute__ ((unused)), double x __attribute__ ((unused)))
+{
+  return 0.0;
+}
+
+double
+__fixdfdi (double x __attribute__ ((unused)), double x __attribute__ ((unused)))
+{
+  return 0.0;
+}
+
+double
+__floatundidf (double x __attribute__ ((unused)), double x __attribute__ ((unused)))
+{
+  return 0.0;
+}
+
