d8663abbb1be0278f65536200e48e6017b6662cd
reSHARMA <reshabhsh@gmail.com>
2020-02-29 22:37:21 +0300

    Format size result as csv

--- benchmark.sh
+++ benchmark.sh
@@ -10,3 +10,7 @@ apt install python3-pyelftools
 
 python3 build_all.py --cc $1 --ld /rvtc/gnu_install/bin/riscv32-unknown-elf-gcc --arch=riscv$2 --chip=size-test-gcc --board=ri5cyverilator
 python3 benchmark_size.py > $3
+
+sed -i -e 's/,//g' $3
+sed -i -e 's/ //g' $3
+head -n 17 $3 >> /rvtc/size.csv
